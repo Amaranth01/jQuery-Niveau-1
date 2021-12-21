e4cde4d1b38c9c973cfc42f3d82ac6e956c2d832
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-21 09:43:03 +0100

    projet ok

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ $('a').text("Un lien est ici en JQuery").attr('href', 'https://google.fr');
 
 $('div').width('80px').height('80px');
 
-$("#bigDiv").children().get(1).append('La deuxième div est remplie');
+$("#bigDiv").children().eq(1).append('La deuxième div est remplie');
