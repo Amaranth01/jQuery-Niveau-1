541216eebc9a98af58bc3e069ce48740ff296e8e
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-21 08:48:46 +0100

    step 3/4

---
+++ app.js
@@ -0,0 +1,7 @@
+$('#id1').text("Hello World");
+
+$('a').text("Un lien est ici en JQuery").attr('href', 'https://google.fr');
+
+$('div').width('80px').height('80px');
+
+$("#bigDiv").children.get(1);
